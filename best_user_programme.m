06286f84ffd0f0e0fe491a5d45ad441b72f6281e
yash9919 <52064558+yash9919@users.noreply.github.com>
2019-06-30 17:36:04 +0530

    Add files via upload

---
+++ best_user_programme.m
@@ -0,0 +1,19 @@
+prompt = 'Enter the number of users ';
+M = input(prompt)
+prompt = 'Enter the number of relay ';
+N=input(prompt)
+prompt = 'Enter the number of iterations ';
+S=input(prompt)
+A=randn(M,S);
+
+%x stores value of max value from each column of A
+x=max(A,[],1)
+
+%loop to add index of highest value of each column in A 
+%Index of highest value of each column is stroed in a new row of each column 
+for i=1:S
+  [r,c]=find(A==x(1,i));
+   A(M+1,i)=r;
+   
+end
+A(M+1,:)=round(A(M+1,:));
